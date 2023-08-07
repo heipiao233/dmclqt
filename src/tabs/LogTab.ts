e90ccf9ad94cf4822636856ccd3bde62539a909f
heipiao233 <heipiao233@outlook.com>
2023-08-07 21:54:27 +0800

    1.2.1

--- src/tabs/LogTab.ts
+++ src/tabs/LogTab.ts
@@ -1,4 +1,4 @@
-import { QGridLayout, QTextBrowser } from "@nodegui/nodegui";
+import { QGridLayout, QPushButton, QTextBrowser } from "@nodegui/nodegui";
 import { ChildProcess } from "child_process";
 import { Tab } from "../tabs";
 
@@ -9,18 +9,27 @@ export default class LogTab extends Tab {
         this.setLayout(layout);
         const text = new QTextBrowser();
         text.setReadOnly(true);
-        process.stdout?.on("data", (data: string) => {
-            for (const i of masks) {
-                data = data.replaceAll(i, "<MASKED>");
-            }
-            text.append(`<pre>${data}</pre>`);
-        });
-        process.stderr?.on("data", (data: string) => {
-            for (const i of masks) {
-                data = data.replaceAll(i, "<MASKED>");
-            }
-            text.append(`<pre style="color: red;">${data}</pre>`);
-        });
+        process.stdout!
+            .on("data", (data: string) => {
+                for (const i of masks) {
+                    data = data.replaceAll(i, "<MASKED>");
+                }
+                text.append(`<pre>${data}</pre>`);
+            });
+
+        process.stderr!
+            .on("data", (data: string) => {
+                for (const i of masks) {
+                    data = data.replaceAll(i, "<MASKED>");
+                }
+
+                text.append(`<pre style="color: red;">${data}</pre>`);
+            });
         layout.addWidget(text);
+
+        const killButton = new QPushButton();
+        killButton.setText("强制结束");
+        killButton.addEventListener("clicked", () => process.kill());
+        layout.addWidget(killButton, 1);
     }
 }
